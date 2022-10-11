105c9a5560985903d6441610a481642283fb18ef
mikel <mikelgoti@gmail.com>
2022-10-11 17:22:07 +0200

    2. COMMIT EN EL BRANCH ENTREGA_1. Correciones para generar las tablas de la base de datos automaticamente y no incluir la carpeta mysql

--- app/php/busqueda_todo.php
+++ app/php/busqueda_todo.php
@@ -22,6 +22,18 @@
     
         $obj = new Database();
         $con = $obj-> conectar();
+
+        $con-> query("CREATE TABLE IF NOT EXISTS todo(
+            id int(100) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            nombre varchar(40) DEFAULT NULL,
+            autor varchar(40) DEFAULT NULL,
+            editorial varchar(40) DEFAULT NULL,
+            genero varchar(40) DEFAULT NULL,
+            publicacion varchar(40) DEFAULT NULL,
+            formato varchar(40) DEFAULT NULL,
+            descarga varchar(500) DEFAULT NULL
+        )");
+        
         if(isset($_POST['b'])){
             echo "HAS BUSCADO ",$_POST['b'];}
         ?>
